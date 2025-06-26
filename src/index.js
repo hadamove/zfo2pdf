5898e2cb1afde9ba3759477b429e1583cdb1ad29
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2025-06-26 14:40:49 +0200

    Remove ads.txt and privacy.html files; update index.html and not_found.html for improved structure and content consistency.

--- src/index.js
+++ src/index.js
@@ -1,78 +1,82 @@
 function onFileInputChanged() {
-    const input = document.getElementById('fileInput');
-    const label = document.querySelector('label[for="fileInput"]');
-    const fileName = input.files.length ? input.files[0].name : null;
-    label.textContent = fileName ? `📑 ${fileName}` : '📤 Nahraj súbor';
+	const input = document.getElementById("fileInput");
+	const label = document.querySelector('label[for="fileInput"]');
+	const fileName = input.files.length ? input.files[0].name : null;
+	label.textContent = fileName ? `📑 ${fileName}` : "📤 Nahraj súbor";
 
-    displayError('');
+	displayError("");
 }
 
 function displayError(message) {
-    const errorContainer = document.getElementById('errorContainer');
-    if (message) {
-        errorContainer.textContent = message;
-        errorContainer.classList.remove('hidden');
-    } else {
-        errorContainer.textContent = '';
-        errorContainer.classList.add('hidden');
-    }
+	const errorContainer = document.getElementById("errorContainer");
+	if (message) {
+		errorContainer.textContent = message;
+		errorContainer.classList.remove("hidden");
+	} else {
+		errorContainer.textContent = "";
+		errorContainer.classList.add("hidden");
+	}
 }
 
 const convertZfoToPdf = () => {
-    const fileInput = document.getElementById('fileInput');
+	const fileInput = document.getElementById("fileInput");
 
-    if (!fileInput.files || fileInput.files.length === 0) {
-        displayError('Nejprve načti soubor, prosím!');
-        return;
-    }
+	if (!fileInput.files || fileInput.files.length === 0) {
+		displayError("Nejprve načti soubor, prosím!");
+		return;
+	}
 
-    const file = fileInput.files[0];
-    const reader = new FileReader();
+	const file = fileInput.files[0];
+	const reader = new FileReader();
 
-    reader.onload = function (event) {
-        try {
-            const zfoContent = event.target.result;
-            const xmlContent = extractXmlContentFromZfo(zfoContent);
-            transformToPdfAndAddLinks(xmlContent);
-        } catch (error) {
-            displayError('Nastala chyba při zpracování souboru. Jde o platný soubor ZFO?');
-        }
-    };
+	reader.onload = (event) => {
+		try {
+			const zfoContent = event.target.result;
+			const xmlContent = extractXmlContentFromZfo(zfoContent);
+			transformToPdfAndAddLinks(xmlContent);
+		} catch (error) {
+			displayError(
+				"Nastala chyba při zpracování souboru. Jde o platný soubor ZFO?",
+			);
+		}
+	};
 
-    reader.onerror = function (_) {
-        displayError('Nastala chyba při čtení souboru. Zkus to prosím znovu.');
-    };
+	reader.onerror = (_) => {
+		displayError("Nastala chyba při čtení souboru. Zkus to prosím znovu.");
+	};
 
-    reader.readAsBinaryString(file);
-}
+	reader.readAsBinaryString(file);
+};
 
 const extractXmlContentFromZfo = (zfoContent) => {
-    const pkcs7Der = forge.util.createBuffer(zfoContent, 'binary');
-    const pkcs7Asn1 = forge.asn1.fromDer(pkcs7Der);
-    const pkcs7 = forge.pkcs7.messageFromAsn1(pkcs7Asn1);
+	const pkcs7Der = forge.util.createBuffer(zfoContent, "binary");
+	const pkcs7Asn1 = forge.asn1.fromDer(pkcs7Der);
+	const pkcs7 = forge.pkcs7.messageFromAsn1(pkcs7Asn1);
 
-    let xmlContentParts = pkcs7.rawCapture.content.value[0].value;
+	const xmlContentParts = pkcs7.rawCapture.content.value[0].value;
 
-    let xmlContent = xmlContentParts.map(part => {
-        let binaryString = part.value;
-        let buffer = new Uint8Array(binaryString.length);
-        for (let i = 0; i < binaryString.length; i++) {
-            buffer[i] = binaryString.charCodeAt(i);
-        }
+	const xmlContent = xmlContentParts
+		.map((part) => {
+			const binaryString = part.value;
+			const buffer = new Uint8Array(binaryString.length);
+			for (let i = 0; i < binaryString.length; i++) {
+				buffer[i] = binaryString.charCodeAt(i);
+			}
 
-        let textDecoder = new TextDecoder("utf-8");
-        return textDecoder.decode(buffer);
-    }).join('');
+			const textDecoder = new TextDecoder("utf-8");
+			return textDecoder.decode(buffer);
+		})
+		.join("");
 
-    if (!xmlContent) {
-        throw new Error('No XML content found in the ZFO file.');
-    }
+	if (!xmlContent) {
+		throw new Error("No XML content found in the ZFO file.");
+	}
 
-    return xmlContent;
-}
+	return xmlContent;
+};
 
 const transformToPdfAndAddLinks = (xmlContent) => {
-    const listStylesheet = `
+	const listStylesheet = `
   <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
     <xsl:output omit-xml-declaration="yes"/>
     <xsl:template match="/">
@@ -82,17 +86,17 @@ const transformToPdfAndAddLinks = (xmlContent) => {
       </xsl:for-each>
     </xsl:template>
   </xsl:stylesheet>`;
-    const fileNames = transformXml(xmlContent, listStylesheet).trim().split('\n');
+	const fileNames = transformXml(xmlContent, listStylesheet).trim().split("\n");
 
-    const container = document.getElementById('downloadLinks');
-    // Clear previous links
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    document.getElementById('result').classList.remove('hidden');
+	const container = document.getElementById("downloadLinks");
+	// Clear previous links
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+	document.getElementById("result").classList.remove("hidden");
 
-    fileNames.forEach((file) => {
-        const dumpStylesheet = `
+	for (const file of fileNames) {
+		const dumpStylesheet = `
     <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
       <xsl:output omit-xml-declaration="yes"/>
       <xsl:template match="/">
@@ -100,38 +104,43 @@ const transformToPdfAndAddLinks = (xmlContent) => {
       </xsl:template>
     </xsl:stylesheet>`;
 
-        const encodedContent = transformXml(xmlContent, dumpStylesheet);
-        const decodedContent = atob(encodedContent);
-        const byteArray = new Uint8Array(decodedContent.length);
-        for (let i = 0; i < decodedContent.length; i++) {
-            byteArray[i] = decodedContent.charCodeAt(i);
-        }
+		const encodedContent = transformXml(xmlContent, dumpStylesheet);
+		const decodedContent = atob(encodedContent);
+		const byteArray = new Uint8Array(decodedContent.length);
+		for (let i = 0; i < decodedContent.length; i++) {
+			byteArray[i] = decodedContent.charCodeAt(i);
+		}
 
-        const blob = new Blob([byteArray], { type: "application/pdf" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = file;
-        a.textContent = `📄 ${file}`;
-        a.className = "block w-full py-2 px-4 text-emerald-500 bg-emerald-100 rounded-md hover:bg-emerald-200 transition duration-150 ease-in-out mt-2 flex gap-2 fade-in-pop ";
+		const blob = new Blob([byteArray], { type: "application/pdf" });
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement("a");
+		a.href = url;
+		a.download = file;
+		a.textContent = `📄 ${file}`;
+		a.className =
+			"block w-full py-2 px-4 text-emerald-500 bg-emerald-100 rounded-md hover:bg-emerald-200 transition duration-150 ease-in-out mt-2 flex gap-2 fade-in-pop ";
 
-        container.appendChild(a);
-    });
-}
+		container.appendChild(a);
+	}
+};
 
 const transformXml = (xmlContent, xsltString) => {
-    const parser = new DOMParser();
-    const xsltProcessor = new XSLTProcessor();
+	const parser = new DOMParser();
+	const xsltProcessor = new XSLTProcessor();
 
-    const xslStylesheet = parser.parseFromString(xsltString, "application/xml");
-    xsltProcessor.importStylesheet(xslStylesheet);
+	const xslStylesheet = parser.parseFromString(xsltString, "application/xml");
+	xsltProcessor.importStylesheet(xslStylesheet);
 
-    const xmlDoc = parser.parseFromString(xmlContent, "application/xml");
-    const fragment = xsltProcessor.transformToFragment(xmlDoc, document);
+	const xmlDoc = parser.parseFromString(xmlContent, "application/xml");
+	const fragment = xsltProcessor.transformToFragment(xmlDoc, document);
 
-    const serializer = new XMLSerializer();
-    return serializer.serializeToString(fragment);
-}
+	const serializer = new XMLSerializer();
+	return serializer.serializeToString(fragment);
+};
 
-document.getElementById('fileInput').addEventListener('change', onFileInputChanged);
-document.getElementById('convertButton').addEventListener('click', convertZfoToPdf);
+document
+	.getElementById("fileInput")
+	.addEventListener("change", onFileInputChanged);
+document
+	.getElementById("convertButton")
+	.addEventListener("click", convertZfoToPdf);
